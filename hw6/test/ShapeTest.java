03e6d9eca9738adbf557f6af67348df69006c5c4
Jingyi-MASY <JJ2905@nyu.edu>
2021-04-05 17:12:18 -0400

    test

--- hw6/test/ShapeTest.java
+++ hw6/test/ShapeTest.java
@@ -13,79 +13,95 @@ public class ShapeTest {
 
   @Before
   public void setup() {
-    c1 = new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 30, 50, 10);
-    o1 = new Oval("o1", ShapeType.OVAL, new Point2D(15, -15), Color.WHITE, 5, 5, 4, 6);
-    r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50), Color.WHITE, 0, 20, 30, 20);
+    c1 = new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0),
+            Color.BLACK, 30, 50, 10);
+    o1 = new Oval("o1", ShapeType.OVAL, new Point2D(15, -15),
+            Color.WHITE, 5, 5, 4, 6);
+    r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50),
+            Color.WHITE, 0, 20, 30, 20);
   }
 
   @Test
   public void testShapeConstructor() {
-    IShape c1 = new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 30, 50, 10);
-    IShape o1 = new Oval("o1", ShapeType.OVAL, new Point2D(15, -15), Color.WHITE, 5, 5, 4, 6);
-    IShape r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50), Color.WHITE, 0, 20, 30, 20);
+    IShape c1 = new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0),
+            Color.BLACK, 30, 50, 10);
+    IShape o1 = new Oval("o1", ShapeType.OVAL, new Point2D(15, -15),
+            Color.WHITE, 5, 5, 4, 6);
+    IShape r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50),
+            Color.WHITE, 0, 20, 30, 20);
 
     //appear after disappear
     try {
-      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 20, 10, 5);
+      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0),
+              Color.BLACK, 20, 10, 5);
       fail("Accepting invalid time");
     } catch (IllegalArgumentException ignored) {
     }
 
     //negative time
     try {
-      new Oval("c1", ShapeType.OVAL, new Point2D(0, 0), Color.BLUE, -10, 10, 20, 15);
+      new Oval("c1", ShapeType.OVAL, new Point2D(0, 0),
+              Color.BLUE, -10, 10, 20, 15);
       fail("Accepting invalid time");
     } catch (IllegalArgumentException ignored) {
     }
 
     //Check null
     try {
-      new Rectangle("c1", null, new Point2D(0, 0), Color.BLACK, 10, 20, 10, 20);
+      new Rectangle("c1", null, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 10, 20);
       fail("Accepting null");
     } catch (IllegalArgumentException ignored) {
     }
 
     try {
-      new Circle(null, ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 10, 20, 200);
+      new Circle(null, ShapeType.CIRCLE, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 200);
       fail("Accepting null");
     } catch (IllegalArgumentException ignored) {
     }
 
     try {
-      new Circle("c1", ShapeType.CIRCLE, null, Color.BLACK, 10, 20, 50);
+      new Circle("c1", ShapeType.CIRCLE, null,
+              Color.BLACK, 10, 20, 50);
       fail("Accepting null");
     } catch (IllegalArgumentException ignored) {
     }
     try {
-      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), null, 10, 20, 50);
+      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0),
+              null, 10, 20, 50);
       fail("Accepting null");
     } catch (IllegalArgumentException ignored) {
     }
 
     //Circle parameter
     try {
-      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 10, 20, 0);
+      new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 0);
       fail("Accepting invalid radius");
     } catch (IllegalArgumentException ignored) {
     }
 
     //Triangle parameter
     try {
-      new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(0, 0), Color.BLACK, 10, 20, 0, 5);
+      new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 0, 5);
       fail("Accepting invalid WH");
     } catch (IllegalArgumentException ignored) {
     }
 
     //Oval parameter
     try {
-      new Oval("r1", ShapeType.OVAL, new Point2D(0, 0), Color.BLACK, 10, 20, 0, 5);
+      new Oval("r1", ShapeType.OVAL, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 0, 5);
       fail("Accepting invalid Oval radius");
     } catch (IllegalArgumentException ignored) {
     }
 
     //Wrong Shape Type
     try {
-      new Rectangle("r1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 10, 20, 5, 5);
+      new Rectangle("r1", ShapeType.CIRCLE, new Point2D(0, 0),
+              Color.BLACK, 10, 20, 5, 5);
       fail("Accepting wrong type");
     } catch (IllegalArgumentException ignored) {
     }
@@ -93,19 +109,16 @@ public class ShapeTest {
 
   @Test
   public void testGet() {
-    IShape c1 = new Circle("c1", ShapeType.CIRCLE, new Point2D(0, 0), Color.BLACK, 30, 50, 10);
-    IShape o1 = new Oval("o1", ShapeType.OVAL, new Point2D(15, -15), Color.WHITE, 5, 5, 4, 6);
-    IShape r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50), Color.WHITE, 0, 20, 30, 20);
     assertEquals("c1", c1.getName());
     assertEquals(ShapeType.RECTANGLE, r1.getType());
     assertEquals(30, c1.getAppearTime());
     assertEquals(5, o1.getDisappearTime());
     assertEquals("c1", c1.getCopy(40).getName());
     assertEquals(new Point2D(30, 50), c1.getCopy(40).getPositionAt(40));
-    try{
+    try {
       c1.getCopy(40).getPositionAt(30);
       fail("Out of copy time boundary");
-    } catch (IllegalArgumentException ignored){
+    } catch (IllegalArgumentException ignored) {
     }
   }
 
@@ -159,14 +172,13 @@ public class ShapeTest {
       fail("Accepting Invalid scale factor");
     } catch (IllegalArgumentException ignored) {
     }
-    assertEquals("scales from Radius: 10 to Radius: 15", c1.getDimensionChange(45, 2));
+    assertEquals("scales from Radius: 10 to Radius: 15",
+            c1.getDimensionChange(45, 2));
 
   }
 
   @Test
   void testMove() {
-    IShape r1 = new Rectangle("r1", ShapeType.RECTANGLE, new Point2D(50, 50),
-            Color.WHITE, 0, 20, 30, 20);
     IShape r1Copy = r1.getCopy(5);
     r1.addMove(new Point2D(10, 10), 11, 16);
     r1.addMove(new Point2D(70, 90), 0, 10);

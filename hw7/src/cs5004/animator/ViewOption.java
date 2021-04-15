4ac1c95f9327ff0be4084de079d6317ade1e460a
Jingyi-MASY <JJ2905@nyu.edu>
2021-04-14 22:42:34 -0400

    Enum ViewOption

---
+++ hw7/src/cs5004/animator/ViewOption.java
@@ -0,0 +1,20 @@
+package cs5004.animator;
+
+public enum ViewOption {
+  TEXT("text"),
+  VISUAL("visual"),
+  SVG("svg");
+  private final String txt;
+
+  /**
+   * This constructor creates view options
+   *
+   * @param txt passed in text indicating which view needs to be created
+   */
+  ViewOption(String txt) {
+    this.txt = txt;
+  }
+  protected boolean isView(String string){
+    return this.txt.equalsIgnoreCase(string);
+  }
+}
